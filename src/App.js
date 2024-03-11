db7c9097d8f05f566a3a9ed97b366078355cff2e
Ananay Mahajan <ananaymahajan2699@gmail.com>
2024-03-11 12:42:46 +0530

    updated the code

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import appStore from "./utils/appStore";
 
 function App() {
   return (
-    <div className="w-[100vw] h-[100vh] ">
+    <div className="h-[100vh] ">
       <Provider store={appStore}>
         <Body />
       </Provider>
